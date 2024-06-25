0d23dc36346f1b5c898006d5b20dbf12d4361aff
Thanooj <thanu1495@gmail.com>
2024-06-25 15:52:51 +0530

    api

--- api.js
+++
@@ -1,4 +0,0 @@
-// VITE_REACT_APP_API_URL=https://api.openweathermap.org/data/2.5
-// VITE_REACT_APP_API_KEY=bb3abfbd0870985604b23cf158cf8a33
-// VITE_REACT_APP_ICON_URL = https://openweathermap.org/img/w
-// VITE_MAPBOXTOKEN=pk.eyJ1IjoicmFtZWVzbW9oZCIsImEiOiJjbG4xZ281djIwMHJ1MnFvaHBuZDV5ZWVzIn0.B39GohhPOcIvsb33_BDXGA
